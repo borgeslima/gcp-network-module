10d18d365bbefede5a2e394cdfbf14cf5abf3eb7
borgeslima <gabrielborges.web@gmail.com>
2024-04-16 12:44:47 -0300

    feat(fix:outputs)

--- tf-outputs.tf
+++ tf-outputs.tf
@@ -1,24 +1,25 @@
-output "network_name" {
+output "id" {
+  value = google_compute_network.this.id
+  description = "Id of network"
+}
+
+
+output "name" {
   value = google_compute_network.this.name
   description = "Name of network"
 }
 
-output "network_description" {
+output "description" {
   value = google_compute_network.this.description
   description = "Description of network"
 }
 
-output "network_id" {
-  value = google_compute_network.this.id
-  description = "id of network"
-}
-
-output "network_internal_ipv6_range" {
+output "internal_ipv6_range" {
   value = google_compute_network.this.internal_ipv6_range
   description = "internal_ipv6_range of network"
 }
 
-output "network_gateway_ipv4" {
+output "gateway_ipv4" {
   value = google_compute_network.this.gateway_ipv4
   description = "gateway_ipv4 of network"
 }
@@ -28,11 +29,15 @@ output "auto_create_subnetworks" {
   description = "auto_create_subnetworks of network"
 }
 
+output "subnetworks" {
+  value = toset([for subnetwork in google_compute_subnetwork.this: subnetwork.name])
+  description = "List of subnetworks"
+}
+
 output "subnetwork_range_names" {
   value = toset([for subnet in google_compute_subnetwork.this: [for secondary in subnet.secondary_ip_range : secondary.range_name]])
   description = "List of Subnetworks of network"
 }
-
 
 
 
